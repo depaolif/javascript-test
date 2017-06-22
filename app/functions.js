dc9b621eae11ac39262fa276a1c770a2dd81ac75
Frederico De Paoli <frederico.de.paoli@gmail.com>
2017-06-22 17:17:01 -0400

    finished functions

--- app/functions.js
+++ app/functions.js
@@ -16,7 +16,7 @@ exports.functionsAnswers = {
   },
 
   makeClosures: function(arr, fn) {
-    return arr.map(fn)
+    return arr.map((el) => fn.bind(null,el))
   },
 
   partial: function(fn, str1, str2) {
@@ -28,14 +28,20 @@ exports.functionsAnswers = {
   },
 
   callIt: function(fn) {
-    return fn.apply(null,arguments)
+    return fn.apply(null,Array.from(arguments).slice(1))
   },
 
   partialUsingArguments: function(fn) {
-    return fn.bind(null,arguments)
+    return fn.bind.apply(fn, [null].concat(Array.from(arguments).slice(1)))
   },
 
   curryIt: function(fn) {
-    return fn.bind(null,arguments[0])
+    return function(x) {
+      return function(y) {
+        return function(z) {
+          return fn(x,y,z)
+        }
+      }
+    }
   }
 };
