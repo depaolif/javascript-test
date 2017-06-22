18a738039f2e6d46bbf92c8aa4d2fab8f43f0e70
Frederico De Paoli <frederico.de.paoli@gmail.com>
2017-06-22 14:37:14 -0400

    finished strings, objects

--- app/functions.js
+++ app/functions.js
@@ -2,38 +2,40 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.functionsAnswers = {
   argsAsArray: function(fn, arr) {
-
+    return fn.apply(null,arr)
   },
 
   speak: function(fn, obj) {
-
+    return fn.apply(obj)
   },
 
   functionFunction: function(str) {
-
+    return function(str2) {
+      return str + ", " + str2
+    }
   },
 
   makeClosures: function(arr, fn) {
-
+    return arr.map(fn)
   },
 
   partial: function(fn, str1, str2) {
-
+    return fn.bind(null,str1,str2)
   },
 
   useArguments: function() {
-
+    return Array.from(arguments).reduce((acc,el) => acc + el,0)
   },
 
   callIt: function(fn) {
-
+    return fn.apply(null,arguments)
   },
 
   partialUsingArguments: function(fn) {
-
+    return fn.bind(null,arguments)
   },
 
   curryIt: function(fn) {
-
+    return fn.bind(null,arguments[0])
   }
 };
--- app/strings.js
+++ app/strings.js
@@ -2,14 +2,49 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.stringsAnswers = {
   reduceString: function(str, amount) {
-
+  	let newString = ''
+    let substr = ''
+    for (let i in str) {
+      if (str[i] === str[i-1]) {
+        if (substr.length < amount) substr += str[i]
+      } else {
+        newString += substr
+        substr = '' + str[i]
+      }
+    }
+    newString += substr
+    return newString
   },
 
   wordWrap: function(str, cols) {
+    // looks ahead to see if there will be another space before a line break is needed
+    function spaceAhead(str, i, cols, currentCol) {
+      const substr = str.slice(
+        parseInt(i,10) + 1,
+        parseInt(i,10) + 1 + cols - currentCol.length
+      )
+      return substr.includes(' ')
+    }
 
+  	let newString = ''
+  	let currentCol = ''
+  	for (let i in str) {
+  		if (str[i] === ' ' && !spaceAhead(str, i, cols, currentCol)) {
+  			newString += currentCol + '\n'
+  			currentCol = ''
+  		} else {
+  			currentCol += str[i]
+  		}
+  	}
+  	newString += currentCol
+  	return newString
   },
 
   reverseString: function(str) {
-
+  	let newString = ''
+  	for (let i = str.length-1; i >= 0; i--) {
+  		newString += str[i]
+  	}
+  	return newString
   }
 };
