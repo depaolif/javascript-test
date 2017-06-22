0f79ccfe579a268f66c979a790c9dc6198c38873
Frederico De Paoli <frederico.de.paoli@gmail.com>
2017-06-22 18:37:17 -0400

    recursion completed

--- app/recursion.js
+++ app/recursion.js
@@ -2,7 +2,18 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.recursionAnswers = {
   listFiles: function(data, dirName) {
-
+  	if (typeof data === 'string' && !dirName) {
+  		return [data]
+  	} else if (typeof data === 'string' && dirName) {
+  		return []
+  	} else {
+  		let dir = !dirName || dirName === data["dir"] ? undefined : dirName
+  		let arr = [], files = data["files"]
+  		for (var i in files) {
+  			arr = arr.concat(recursionAnswers.listFiles(files[i],dir))
+  		}
+  		return arr
+  	}
   },
 
   permute: function(arr) {
