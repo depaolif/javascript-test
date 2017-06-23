6ee246c55f45a34315c65e0f921352bce17793a6
Frederico De Paoli <frederico.de.paoli@gmail.com>
2017-06-23 00:36:51 -0400

    added extra recursions

--- app/recursion.js
+++ app/recursion.js
@@ -17,14 +17,59 @@ exports.recursionAnswers = {
   },
 
   permute: function(arr) {
+    let perms = []
+    for (let i = 0; i < arr.length; i++) {
+      let subArr = arr.slice(0,i).concat(arr.slice(i+1))
+      let rem = recursionAnswers.permute(subArr)
+      if (rem.length === 0) {
+        perms.push([arr[i]])
+      } else {
+        for (let j = 0; j < rem.length; j++) {
+          perms.push([arr[i]].concat(rem[j]))
+        }
+      }
+    }
 
+    return perms
   },
 
   fibonacci: function(n) {
-
+    if (n === 0) return 0
+    if (n === 1) return 1
+    return recursionAnswers.fibonacci(n-1) + recursionAnswers.fibonacci(n-2)
   },
 
   validParentheses: function(n) {
+    // simple recursive function that works on only parens
+    if (!n.includes('(') && !n.includes(')')) {
+      return true
+    } else if (n[0] === '(' && n.includes(')')) {
+      n = n.replace('(','').replace(')','')
+      return recursionAnswers.validParentheses(n)
+    } else {
+      return false
+    }
 
+    // or a non-recursive function that works on complicated strings, implements a stack
+    // let parens = [], bad = false
+    // for (let i in n) {
+    //   switch(n[i]) {
+    //     case '(':
+    //     case '[':
+    //     case '{':
+    //       parens.push(n[i])
+    //       break
+    //     case ')':
+    //       parens[parens.length-1] === '(' ? parens.pop() : bad = true
+    //       break
+    //     case ']':
+    //       parens[parens.length-1] === '[' ? parens.pop() : bad = true
+    //       break
+    //     case '}':
+    //       parens[parens.length-1] === '{' ? parens.pop() : bad = true
+    //   }
+    //   if (bad) return false
+    // }
+    // return parens.length === 0
   }
 };
--- tests/app/recursion.js
+++ tests/app/recursion.js
@@ -48,4 +48,32 @@ describe('recursion', function() {
     expect(result.indexOf('main.js') > -1).to.be.ok;
     expect(result.indexOf('underscore.js') > -1).to.be.ok;
   });
+
+  it('you should be able to return all permutations of an array', function() {
+    expect(recursionAnswers.permute(['a','b','c'])).to.eql([
+      ["a", "b", "c"],
+      ["a", "c", "b"],
+      ["b", "a", "c"],
+      ["b", "c", "a"],
+      ["c", "a", "b"],
+      ["c", "b", "a"]])
+  })
+
+  it('you should be able to return the fibonacci number in nth place', function() {
+    var fibNumbers = [0,1,1,2,3,5,8,13,21,34,55,89,144,233,377,610,987]
+    expect(recursionAnswers.fibonacci(0)).to.eql(fibNumbers[0])
+    expect(recursionAnswers.fibonacci(3)).to.eql(fibNumbers[3])
+    expect(recursionAnswers.fibonacci(8)).to.eql(fibNumbers[8])
+    expect(recursionAnswers.fibonacci(11)).to.eql(fibNumbers[11])
+    expect(recursionAnswers.fibonacci(16)).to.eql(fibNumbers[16])
+  })
+
+  it('you should be able to return whether the parenthesis set is balanced', function() {
+    expect(recursionAnswers.validParentheses('()')).to.be.ok
+    expect(recursionAnswers.validParentheses('(())')).to.be.ok
+    expect(recursionAnswers.validParentheses('()()')).to.be.ok
+    expect(recursionAnswers.validParentheses('(()(')).not.to.be.ok
+    expect(recursionAnswers.validParentheses('(')).not.to.be.ok
+
+  })
 });
